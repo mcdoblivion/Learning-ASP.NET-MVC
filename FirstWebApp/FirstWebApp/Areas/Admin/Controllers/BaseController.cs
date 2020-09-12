faf02c8e7b03e5235bf63bcb0de3138e6c904d14
mcd_oblivion <mcd.oblivion@gmail.com>
2020-09-12 11:41:56 +0700

    Check session in Admin login

---
+++ FirstWebApp/FirstWebApp/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using FirstWebApp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FirstWebApp.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Admin/Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (UserLogin)Session[CommonConst.USER_SESSION];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(
+                        new { controller = "Login", action = "Index", Area = "Admin" }));
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
