237f178a90d0c22d7481bdbc05bd4c0faee27c48
mcd_oblivion <mcd.oblivion@gmail.com>
2020-10-07 23:30:48 +0700

    Section 2 - Action Result

--- Vidly/Vidly/App_Start/BundleConfig.cs
+++ Vidly/Vidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-materia.css",
                       "~/Content/site.css"));
         }
     }
