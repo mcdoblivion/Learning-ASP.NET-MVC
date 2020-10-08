d9e3d3f21c4df0c2fe0ade68addeff2409a4ea77
mcd_oblivion <mcd.oblivion@gmail.com>
2020-10-08 12:40:09 +0700

    Section 2 - Routing

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
-    public class MovieController : Controller
+    public class MoviesController : Controller
     {
         // GET: Movie/Random
         public ActionResult Random()
@@ -32,6 +29,12 @@ namespace Vidly.Controllers
                 sortBy = "Name";
             }
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(month + "/" + year);
         }
     }
 }
