b78dc6b078e7409fbca9483793245f21075cef60
mcd_oblivion <mcd.oblivion@gmail.com>
2020-10-18 18:06:44 +0700

    Section 3 - Update Name of Membership Type

---
+++ Vidly/Vidly/Migrations/202010181018300_UpdateNameOfMembershipType.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class UpdateNameOfMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET Name = 'Far As You Go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Silver' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Gold' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Diamond' WHERE Id = 4");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
