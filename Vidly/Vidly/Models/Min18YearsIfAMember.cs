7e807b5c4d18ebd43e3c7fa11c13c4d6f96923e9
mcd_oblivion <mcd.oblivion@gmail.com>
2020-10-20 23:11:22 +0700

    Section 5 - Refactoring Magic Numbers

--- Vidly/Vidly/Models/Min18YearsIfAMember.cs
+++ Vidly/Vidly/Models/Min18YearsIfAMember.cs
@@ -11,7 +11,7 @@ namespace Vidly.Models
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var customer = (Customer)validationContext.ObjectInstance;
-            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1) return ValidationResult.Success;
+            if (customer.MembershipTypeId == MembershipType.Unknown || customer.MembershipTypeId == MembershipType.PayAsYouGo) return ValidationResult.Success;
             if (customer.DateOfBirth == null) return new ValidationResult("Birthday is required.");
             var age = DateTime.Today.Year - customer.DateOfBirth.Value.Year;
             return age >= 18
