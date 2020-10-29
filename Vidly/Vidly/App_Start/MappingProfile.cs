ff265ecbfcdcf6e30cc5d1df1c5b279cb9733cf0
mcd_oblivion <mcd.oblivion@gmail.com>
2020-10-29 23:25:27 +0700

    Section 6 - Exercise

--- Vidly/Vidly/App_Start/MappingProfile.cs
+++ Vidly/Vidly/App_Start/MappingProfile.cs
@@ -12,8 +12,15 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
+            //Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public MovieDto()
+        {
+            AddedDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
