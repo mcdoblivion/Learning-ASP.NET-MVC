370e381b5f586ae91c1d3845ddc11c4d9644922c
mcd_oblivion <mcd.oblivion@gmail.com>
2020-10-30 12:51:39 +0700

    Section 7 - Bootbox

--- Vidly/Vidly/App_Start/BundleConfig.cs
+++ Vidly/Vidly/App_Start/BundleConfig.cs
@@ -19,7 +19,8 @@ namespace Vidly
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
